a63c78b70ee001cd6ca88ee7b24258283af17910
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-09-04 20:52:24 -0700

    post style

--- client/post/Post.js
+++ client/post/Post.js
@@ -5,33 +5,68 @@ import CardContent from '@material-ui/core/CardContent'
 import CardActions from '@material-ui/core/CardActions'
 import Typography from '@material-ui/core/Typography'
 import Avatar from '@material-ui/core/Avatar'
+import {makeStyles} from '@material-ui/core/styles'
 import IconButton from '@material-ui/core/IconButton'
 import FavoriteIcon from '@material-ui/icons/Favorite'
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
 import {remove, like, unlike} from './api-post.js'
 
+const useStyles = makeStyles(theme => ({
+    card: {
+      maxWidth:600,
+      margin: 'auto',
+      marginBottom: theme.spacing(3),
+      backgroundColor: 'rgba(0, 0, 0, 0.06)'
+    },
+    cardContent: {
+      backgroundColor: 'white',
+      padding: `${theme.spacing(2)}px 0px`
+    },
+    cardHeader: {
+      paddingTop: theme.spacing(1),
+      paddingBottom: theme.spacing(1)
+    },
+    text: {
+      margin: theme.spacing(2)
+    },
+    photo: {
+      textAlign: 'center',
+      backgroundColor: '#f2f5f4',
+      padding:theme.spacing(1)
+    },
+    media: {
+      height: 200
+    },
+    button: {
+     margin: theme.spacing(1),
+    }
+  }))
+
+  
 export default function Post({post}) {
-    console.log(post)
+    const classes = useStyles()
     const { created, likes, photo, text, postedBy, _id } = post
     const clickLike = () => {
         console.log('like')
     }
     return (
-        <Card>
+        <Card className={classes.card}>
         <CardHeader
             avatar={
               <Avatar src={'/api/users/photo/'+postedBy._id}/>
             }
             title={postedBy.name}
             subheader={(new Date(created)).toDateString()}
+            className={classes.cardHeader}
           />
-        <CardContent>
-          <Typography component="p">
+        <CardContent className={classes.cardContent}>
+          <Typography component="p" className={classes.text} >
             {text}
           </Typography>
           {photo &&
-            (<div>
+            (<div className={classes.photo}>
               <img
+                className={classes.media}
                 src={'/api/posts/photo/'+_id}
                 />
             </div>)}
